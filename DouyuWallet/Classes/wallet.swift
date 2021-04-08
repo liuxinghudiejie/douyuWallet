9275f66e9e69281aef1fd233c72887e01568f4ca
yangxin <3304567487@qq.com>
2021-04-08 16:52:57 +0800

    提交

---
+++ DouyuWallet/Classes/wallet.swift
@@ -0,0 +1,14 @@
+//
+//  wallet.swift
+//  
+//
+//  Created by douyu on 2021/4/8.
+//
+
+import Foundation
+
+class Wallet {
+    func test(){
+        print("test")
+    }
+}
---
+++ Example/Pods/Target Support Files/DouyuWallet/DouyuWallet-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DouyuWallet : NSObject
+@end
+@implementation PodsDummy_DouyuWallet
+@end
